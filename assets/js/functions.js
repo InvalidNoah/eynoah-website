a06804e138b27d0e0ca447aaf1b75e3a564d491e
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 11:37:03 +0200

    ok

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,6 +4,31 @@ function getDiscordLink(){
         document.getElementById('dc').innerHTML = "<p class='dc'>.gg/universex</p>";
         setTimeout(() => {
             document.getElementById('dc').innerHTML = "";
+            setTimeout(() => {
+                window.open('https://discord.gg/Zt3TfwjXsR', '_blank')
+            }, 1000);
         }, 1000);
     }, 1000);
+}
+
+function myGithub(){
+    document.getElementById('github').innerHTML = "<span class='loading loading-bars loading-xs'></span>";
+    setTimeout(() => {
+        document.getElementById('github').innerHTML = "<p class='git'>Leite";
+        setTimeout(() => {
+            document.getElementById('github').innerHTML = "<p class='git'> weiter....";
+            setTimeout(() => {
+                document.getElementById('github').innerHTML = "";
+                setTimeout(() => {
+                    window.open('https://github.com/InvalidNoah', '_blank')
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+};
+
+function title(){
+    setInterval(() => {
+        document.innerHTML = "<title>EyNoah best Site EU WEst";
+    }, 5000);
 }
