381c033270b4cb375d58b84bbef2bcb0ef7e3ab5
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 11:21:13 +0200

    lol

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,9 @@
 function getDiscordLink(){
-    document.getElementById("dc").innerHTML = "<p class='dc'>.gg/UniverseX";
+    document.getElementById("dc").innerHTML = "<span class='loading loading-bars loading-xs'></span>";
     setTimeout(() => {
-        document.getElementById('dc').innerHTML = "";
+        document.getElementById('dc').innerHTML = "<p class='dc'>.gg/universex</p>";
+        setTimeout(() => {
+            document.getElementById('dc').innerHTML = "";
+        }, 1000);
     }, 1000);
 }
